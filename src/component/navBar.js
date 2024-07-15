cafeb7364b7cbbeb4b688983a40e60a6b7fedd46
Prerna Tanwar <98750278+PT-02@users.noreply.github.com>
2024-07-15 18:53:39 +0530

    navMobileview

--- src/component/navBar.js
+++ src/component/navBar.js
@@ -1,19 +1,26 @@
-// App.js
-
-import React from 'react';
-import '../Style/navbar.css' ;
+import React, { useState } from 'react';
+import '../Style/navbar.css';
 import { Link } from 'react-router-dom';
 
-
-
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <div className="navBar">
         <div className="navLeft">
           Constructs
         </div>
-        <div className="navRight">
+        <div className="hamburger" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`navRight ${isOpen ? 'active' : ''}`}>
           <div className="content">
             <Link to='/' style={{ color: 'black', textDecoration: 'none' }}>Home</Link>
           </div>
@@ -21,7 +28,7 @@ function NavBar() {
             <Link to='/#service' style={{ color: 'black', textDecoration: 'none' }}>Service</Link>
           </div>
           <div className="content">
-          <Link to='/Project' style={{ color: 'black', textDecoration: 'none' }}>Project</Link>
+            <Link to='/Project' style={{ color: 'black', textDecoration: 'none' }}>Project</Link>
           </div>
           <div className="content">
             <Link to='/AboutUs' style={{ color: 'black', textDecoration: 'none' }}>About Us</Link>
@@ -31,8 +38,6 @@ function NavBar() {
           </div>
         </div>
       </div>
-
-      
     </>
   );
 }
